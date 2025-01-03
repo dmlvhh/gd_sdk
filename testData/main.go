39903c0f666726e88975ac9c9b40d48bffa6ad0e
eternal <1967798998@qq.com>
2025-01-03 18:17:56 +0800

    feature:gd更新

--- testData/main.go
+++ testData/main.go
@@ -8,9 +8,12 @@ import (
 
 func main() {
 	gd := gd_sdk.NewConfig("https://122.192.50.42:11060", "1847483747142742018", "QRAXmCI0DuJOvucX2Kh9FiyGZA38Eoba", "5xYx0jWdp9KE8UVsWK4y2UDBUlc8w14s")
-	res, err := gd.BatchQuerySimFlow(&gd_sdk.BatchQuerySimFlowReq{
-		Iccids:    []string{"89861590082420569601"},
-		QueryDate: "202501",
+	//res, err := gd.BatchQuerySimFlow(&gd_sdk.BatchQuerySimFlowReq{
+	//	Iccids:    []string{"89861590092420074996"},
+	//	QueryDate: "202501",
+	//})
+	res, err := gd.QueryBasicInfoBatch(&gd_sdk.QueryBasicInfoBatchReq{
+		Iccids: []string{"89861590082420210118"},
 	})
 	if err != nil {
 		return
